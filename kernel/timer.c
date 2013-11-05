2b6e942d14979c0665c261c620d984edcaebd985
Dennis Rassmann <showp1984@gmail.com>
2013-11-05 19:43:34 +0100

    cpuinit: get rid of __cpuinit, third regexp This commit is the result of:
    find -name "*.c" | xargs sed -i "s/ __cpuinitdata//g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	arch/arm/kernel/perf_event.c
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    
    	kernel/timer.c

--- kernel/timer.c
+++ kernel/timer.c
@@ -1647,7 +1647,7 @@ static int init_timers_cpu(int cpu)
 {
 	int j;
 	struct tvec_base *base;
-	static char __cpuinitdata tvec_base_done[NR_CPUS];
+	static char tvec_base_done[NR_CPUS];
 	unsigned long flags;
 
 	if (!tvec_base_done[cpu]) {
@@ -1777,7 +1777,7 @@ static int timer_cpu_notify(struct notifier_block *self,
 	return NOTIFY_OK;
 }
 
-static struct notifier_block __cpuinitdata timers_nb = {
+static struct notifier_block timers_nb = {
 	.notifier_call	= timer_cpu_notify,
 };
 
