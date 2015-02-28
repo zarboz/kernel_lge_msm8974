a35cca656d8fa1857a68b6978b2edfb0307aaa8f
Ethan <cannondalev2000@gmail.com>
2015-02-27 17:15:35 -0800

    Revert "update to use values from UBER kernel"
    This reverts commit 90340e8d815b329140df4b276183f958a25dbf03.

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -946,8 +946,8 @@ static void __init bus_init(const struct l2_level *l2_level)
 		dev_err(drv.dev, "initial bandwidth req failed (%d)\n", ret);
 }
 
-#define MAX_VDD 1200
-#define MIN_VDD 500
+#define MAX_VDD 1300
+#define MIN_VDD 600
 
 ssize_t acpuclk_get_vdd_levels_str(char *buf)
 {
